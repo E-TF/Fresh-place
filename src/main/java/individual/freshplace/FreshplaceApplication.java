6921929028cba9ac807180cbd02609ab0833093c
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2022-03-29 14:21:55 +0900

    OO

---
+++ src/main/java/individual/freshplace/FreshplaceApplication.java
@@ -0,0 +1,13 @@
+package individual.freshplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshplaceApplication.class, args);
+	}
+
+}

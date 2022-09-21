f536acfde463a297e282b29c409cb42ff57980e3
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2022-09-21 20:26:25 +0900

    코드 테이블 삭제하기, 캐시 유효시간 다시 설정

--- src/main/java/individual/freshplace/util/constant/Cache.java
+++ src/main/java/individual/freshplace/util/constant/Cache.java
@@ -8,10 +8,10 @@ public class Cache {
 
     public static final String CATEGORY = "category";
     public static final String MAIN_CATEGORY_KEY = "'mainCategory'";    //SPEL 표현식
-    public static final long CATEGORY_EXPIRE = 30L;
+    public static final long CATEGORY_EXPIRE = 7L;
 
     public static final String SUB_CATEGORY = "subCategory";
-    public static final long SUB_CATEGORY_EXPIRE = 7L;
+    public static final long SUB_CATEGORY_EXPIRE = 1L;
 
     public static final String ITEMS_BY_CATEGORY = "items";
     public static final long ITEMS_EXPIRE = 1L;

00cdb2f19fc90a06f21f5203f863829181965da1
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2022-11-01 15:11:48 +0900

    build에러는 exception 클래스 추가 해놓고 커밋을 안했기때문.

--- src/main/java/individual/freshplace/util/constant/Cart.java
+++
@@ -1,8 +0,0 @@
-package individual.freshplace.util.constant;
-
-public class Cart {
-
-    public static final String COOKIE_PARAMETER_SEQ = "itemSeq";
-    public static final String COOKIE_PARAMETER_COUNT = "itemCounting";
-    public static final String COOKIE_EMPTY_STRING = "장바구니가 비어있습니다.";
-}

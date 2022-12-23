b11a239935902525da0b58ee00eebf54b513097f
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2022-12-23 19:29:30 +0900

    Controller 를 RestController 로 변경

--- src/main/java/individual/freshplace/controller/AccountController.java
+++ src/main/java/individual/freshplace/controller/AccountController.java
@@ -4,14 +4,14 @@ import individual.freshplace.dto.signup.SignupRequest;
 import individual.freshplace.service.FSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class AccountController {
 
